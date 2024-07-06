001ece490049358a658124aee498a0732d0626b1
Dragan <milosevicdragan002@gmail.com>
2024-07-06 11:26:06 +0200

    Add track order func

--- backend/main.py
+++ backend/main.py
@@ -1,10 +1,16 @@
-from fastapi import FastAPI
-from fastapi import Request
+from fastapi import FastAPI, Request
 from fastapi.responses import JSONResponse
-
+import db_helper
 app = FastAPI()
 
 
+@app.get("/")
+async def read_root():
+    return {"message": "Hello, World!"}
+
+@app.get("/favicon.ico")
+async def read_favicon():
+    return JSONResponse(content={"detail": "Not found"}, status_code=404)
 
 @app.post("/")
 async def handle_request(request: Request):
@@ -14,10 +20,16 @@ async def handle_request(request: Request):
     output_contexts = payload['queryResult']['outputContexts']
     
     if intent == "track.order - context: ongoing-tracking":
-        return JSONResponse(content={
-            "fulfillmentText":f"Received == {intent}== in the backend"
-        })
+        return track_order(parameters)
+
+def track_order(parameters: dict, session_id: str):
+    order_id = int(parameters['order_id'])
+    order_status = db_helper.get_order_status(order_id)
+    if order_status:
+        fulfillment_text = f"The order status for order id: {order_id} is: {order_status}"
     else:
-        return JSONResponse(content={
-            "message":"Not tracking"
-            })
+        fulfillment_text = f"No order found with order id: {order_id}"
+
+    return JSONResponse(content={
+        "fulfillmentText": fulfillment_text
+    })
