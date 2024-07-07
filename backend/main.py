cc2817354fadde35224e132bdae56f1686bc1c4e
Dragan <milosevicdragan002@gmail.com>
2024-07-07 11:52:18 +0200

    Add order func

--- backend/main.py
+++ backend/main.py
@@ -1,29 +1,76 @@
 from fastapi import FastAPI, Request
 from fastapi.responses import JSONResponse
 import db_helper
+import generic_helper
 app = FastAPI()
 
 
-@app.get("/")
-async def read_root():
-    return {"message": "Hello, World!"}
 
-@app.get("/favicon.ico")
-async def read_favicon():
-    return JSONResponse(content={"detail": "Not found"}, status_code=404)
+inprogress_orders = {}
 
 @app.post("/")
 async def handle_request(request: Request):
+    # Retrieve the JSON data from the request
     payload = await request.json()
+
+    
     intent = payload['queryResult']['intent']['displayName']
     parameters = payload['queryResult']['parameters']
     output_contexts = payload['queryResult']['outputContexts']
-    
-    if intent == "track.order - context: ongoing-tracking":
-        return track_order(parameters)
+    session_id = generic_helper.extract_session_id(output_contexts[0]["name"])
 
-def track_order(parameters: dict, session_id: str):
-    order_id = int(parameters['order_id'])
+    intent_handler_dict = {
+        'order.add - context: ongoing-order': add_to_order,
+        #'order.remove - context: ongoing-order': remove_from_order,
+        'order.complete - context: ongoing-order': complete_order,
+        'track.order - context: ongoing-tracking': track_order
+    }
+
+    return intent_handler_dict[intent](parameters, session_id)
+
+def save_to_db(order:dict):
+    next_order_id = db_helper.get_next_order_id()
+    
+    for food_item,quantity in order.items():
+        rcode = db_helper.insert_order_item(food_item,quantity,next_order_id)
+        if rcode == -1:
+            return -1
+        
+    db_helper.insert_order_tracking(next_order_id,"in progress")
+    
+    return next_order_id
+
+
+def complete_order(parameters: dict,session_id: str):
+    pass
+
+def add_to_order(parameters: dict,session_id: str):
+    food_items = parameters["food-item"]
+    quantities = parameters["number"]
+    
+    if len(food_items) != len(quantities):
+        fulfillment_text = "Sorry I didn't understand. Can you please specify food items and quantities clearly?"
+    else:
+        new_food_dict = dict(zip(food_items,quantities))
+        
+        if session_id in inprogress_orders:
+            current_food_dict = inprogress_orders[session_id]
+            current_food_dict.update(new_food_dict)
+            inprogress_orders[session_id] = current_food_dict
+        else:
+            inprogress_orders[session_id] = new_food_dict
+        print("*******************")
+        print(inprogress_orders[session_id])    
+        order_str = generic_helper.get_str_from_food_dict(inprogress_orders[session_id])
+        fulfillment_text = f"So far you have: {order_str}. Do you need anything else?"
+
+    return JSONResponse(content={
+        "fulfillmentText": fulfillment_text
+    })
+    
+    
+def track_order(parameters: dict,session_id: str):
+    order_id = int(parameters['number'])
     order_status = db_helper.get_order_status(order_id)
     if order_status:
         fulfillment_text = f"The order status for order id: {order_id} is: {order_status}"
